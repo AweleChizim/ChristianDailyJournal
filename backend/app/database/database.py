5da35e3b9e9fe2bd9ee607c686e862a0aa692362
Awele Chizim <chizimawele@gmail.com>
2026-07-18 02:49:23 +0200

    (Updates)

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -7,12 +7,17 @@ from app.core.config import settings
 from app.models.user import User
 from app.models.entry import Entry
 from app.models.daily_verse import DailyVerse
+import certifi
+from motor.motor_asyncio import AsyncIOMotorClient
+
 
 
 async def init_db():
 
     client = AsyncIOMotorClient(
-        settings.MONGODB_URL
+        settings.MONGODB_URL,
+        tls=True,
+        tlsCAFile=certifi.where()
     )
 
     database = client[
